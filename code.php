b47fe96ed143eea074b3e3aa11baa951d9093c96
idealprojectuz <idealprojectuz@gmail.com>
2023-04-17 01:05:24 +0500

    code.php

--- code.php
+++ code.php
@@ -1,40 +1,45 @@
 <?php
+require_once 'vendor/autoload.php';
 
-require 'vendor/autoload.php';
-
-use PhpOffice\PhpWord\Element\Field;
-use PhpOffice\PhpWord\Element\Table;
-use PhpOffice\PhpWord\Element\TextRun;
-use PhpOffice\PhpWord\SimpleType\TblWidth;
-
+// Create new Word document
 $templateProcessor = new \PhpOffice\PhpWord\TemplateProcessor('template.docx');
 
-$table = new Table(array('borderSize' => 5, 'borderColor' => 'black', 'width' => 9350, 'unit' => TblWidth::TWIP));
+// Create section and set font style
+$phpWord = new \PhpOffice\PhpWord\PhpWord();
+$section = $phpWord->addSection();
+$fontStyle = ['spaceAfter' => 60, 'size' => 12];
+
+// Add title style
+$phpWord->addTitleStyle(1, ['size' => 14, 'color' => '000000', 'bold' => true]);
+
+// Add TOC to section
+$toc = $section->addTOC($fontStyle);
+$toc->setMinDepth(1);
+$toc->setMaxDepth(1);
+
+// Create table with product name, price and image
+$table = $section->addTable();
 $table->addRow();
-$table->addCell(250)->addText('Salfetka ishlab chiqarish uskunasi \n ishlab chiqarish quvvati 500 dona 24 soatda kuchlanish 220V ');
-$table->addCell(250)->addImage(
-    'qrcode.png',
-    array(
-        'width'         => 100,
-        'height'        => 100,
-        'marginTop'     => -1,
-        'marginLeft'    => -1,
-        'wrappingStyle' => 'behind'
-    )
-);
-$table->addCell(250)->addImage(
-    'qrcode.png',
-    array(
-        'width'         => 100,
-        'height'        => 100,
-        'marginTop'     => -1,
-        'marginLeft'    => -1,
-        'wrappingStyle' => 'behind'
-    )
-);
+$table->addCell(4000)->addText('Product');
+$table->addCell(2000)->addText('Price');
+$table->addCell(4000)->addText('Image');
+$table->addRow();
+$table->addCell(4000)->addText('Product 1');
+$table->addCell(2000)->addText('$19.99');
+$table->addCell(4000)->addImage('http://afex.loc/product1.jpg', ['width' => 200, 'height' => 200]);
+$table->addRow();
+$table->addCell(4000)->addText('Product 2');
+$table->addCell(2000)->addText('$29.99');
+$table->addCell(4000)->addImage('http://afex.loc/product1.jpg', ['width' => 200, 'height' => 200]);
 
+// Get table as XML and insert it into document
+$xmlWriter = new \PhpOffice\PhpWord\Shared\XMLWriter(\PhpOffice\PhpWord\Shared\XMLWriter::STORAGE_MEMORY, './', \PhpOffice\PhpWord\Settings::hasCompatibility());
+$tableWriter = new PhpOffice\PhpWord\Writer\Word2007\Element\Table($xmlWriter, $table);
+$tableWriter->write();
+$tableXml = $xmlWriter->getData();
+\PhpOffice\PhpWord\Settings::setOutputEscapingEnabled(false);
+$templateProcessor->setValue('products', $tableXml);
+\PhpOffice\PhpWord\Settings::setOutputEscapingEnabled(true);
 
-$templateProcessor->setComplexBlock('table', $table);
-
-$pathtoSave = 'order.docx';
-$templateProcessor->saveAs($pathtoSave);
+// Save document as new file
+$templateProcessor->saveAs('output.docx');
